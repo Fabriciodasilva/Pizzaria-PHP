7130e3f8cc8de9fedd768fdc3b6e214cd71d9a10
Fabricio <fabriciodasilvax2@gmail.com>
2024-10-09 09:41:58 -0300

    criando models e migrations

---
+++ app/Models/Pizza.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Pizza extends Model
+{
+    use HasFactory;
+
+    public function order(): BelongsToMany
+    {
+        return $this->belongsToMany(Order::class);
+    }
+}
